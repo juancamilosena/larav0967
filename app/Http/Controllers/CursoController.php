a8ef2c35526b31fd552b3d0aac164755fb1ca027
Johansant22 <johansan799@gmail.com>
2024-02-16 16:31:39 -0500

    primer compromiso

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    //
+    public function hola($num1,$num2){
+
+
+
+        return $num1+$num2;
+    
+       }
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\CursoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/curso/{dato1}/{dato2}',[CursoController::class,'hola'])->name('curso.show');
